cb613a139850de257e37f964a72bb70db77769ef
Brian Seeders <brian.seeders@elastic.co>
2021-06-04 16:36:13 -0400

    Add 7.13 to on-merge branches

--- pipelines/kibana-on-merge.tf
+++ pipelines/kibana-on-merge.tf
@@ -12,7 +12,7 @@ resource "buildkite_pipeline" "on-merge" {
   EOT
 
   default_branch       = "master"
-  branch_configuration = "master 7.x"
+  branch_configuration = "master 7.x 7.13"
 
   provider_settings {
     build_branches      = true
