21142412645a52dbd57bf5964af1f2864e2f998e
Brian Seeders <brian.seeders@elastic.co>
2021-06-07 15:17:09 -0400

    Create plan before apply

--- pipelines/.buildkite/scripts/deploy.sh
+++ pipelines/.buildkite/scripts/deploy.sh
@@ -13,9 +13,18 @@ echo --- terraform init
 docker run -it --rm --init --volume "$(pwd)":/app --workdir /app \
   hashicorp/terraform:"$TERRAFORM_VERSION" init
 
+echo --- terraform plan
+
+docker run -it --rm --init --volume "$(pwd)":/app --workdir /app \
+  -e TF_VAR_buildkite_token -e TF_VAR_github_token \
+  hashicorp/terraform:"$TERRAFORM_VERSION" \
+  plan -input=false -var "github_owner=elastic" -out=plan
+
+buildkite-agent artifact upload plan
+
 echo --- terraform apply
 
 docker run -it --rm --init --volume "$(pwd)":/app --workdir /app \
   -e TF_VAR_buildkite_token -e TF_VAR_github_token \
   hashicorp/terraform:"$TERRAFORM_VERSION" \
-  apply -auto-approve -input=false -var "github_owner=elastic"
+  apply -auto-approve -input=false -var "github_owner=elastic" plan
