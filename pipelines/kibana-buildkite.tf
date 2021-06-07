7cfe83a6e9fd4a7ad202f127f3257c00f281fa32
Brian Seeders <brian.seeders@elastic.co>
2021-06-07 15:09:56 -0400

    Add kibana-buildkite overall repo pipeline

--- pipelines/kibana-buildkite.tf
+++ pipelines/kibana-buildkite.tf
@@ -1,3 +1,28 @@
+resource "buildkite_pipeline" "kibana-buildkite-trigger" {
+  name        = "kibana-buildkite / trigger"
+  description = "Triggers pipelines on commit based on the changed files in the commit"
+  repository  = "https://github.com/elastic/kibana-buildkite.git"
+  steps       = <<-EOT
+  env:
+    SLACK_NOTIFICATIONS_CHANNEL: '#kb-bk'
+    SLACK_NOTIFICATIONS_ENABLED: 'false'
+  steps:
+    - label: ":pipeline: Pipeline upload"
+      command: buildkite-agent pipeline upload .buildkite/pipeline.yml
+  EOT
+
+  default_branch       = "main"
+  branch_configuration = "main"
+
+  provider_settings {
+    build_branches      = true
+    build_tags          = false
+    build_pull_requests = false
+
+    trigger_mode = "code"
+  }
+}
+
 resource "buildkite_pipeline" "kibana-buildkite-pipelines-deploy" {
   name        = "kibana-buildkite / pipelines / deploy"
   repository  = "https://github.com/elastic/kibana-buildkite.git"
