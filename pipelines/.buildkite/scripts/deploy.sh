059944013e6899396a8bcca51444cd1314d209ae
Brian Seeders <brian.seeders@elastic.co>
2021-06-07 15:20:28 -0400

    Remove vars

--- pipelines/.buildkite/scripts/deploy.sh
+++ pipelines/.buildkite/scripts/deploy.sh
@@ -20,11 +20,8 @@ docker run -it --rm --init --volume "$(pwd)":/app --workdir /app \
   hashicorp/terraform:"$TERRAFORM_VERSION" \
   plan -input=false -var "github_owner=elastic" -out=plan
 
-buildkite-agent artifact upload plan
-
 echo --- terraform apply
 
 docker run -it --rm --init --volume "$(pwd)":/app --workdir /app \
-  -e TF_VAR_buildkite_token -e TF_VAR_github_token \
   hashicorp/terraform:"$TERRAFORM_VERSION" \
-  apply -auto-approve -input=false -var "github_owner=elastic" plan
+  apply -auto-approve -input=false plan
