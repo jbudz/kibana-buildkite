ca2f2a4ef9c982454cadf22d4e2b9cab85091645
Brian Seeders <brian.seeders@elastic.co>
2021-08-12 13:23:18 -0400

    Install kubectl via apt

--- agents/setup.sh
+++ agents/setup.sh
@@ -111,7 +111,8 @@ apt-get install --yes \
   rsync \
   openjdk-11-jre-headless \
   unzip \
-  build-essential
+  build-essential \
+  kubectl
 
 apt-get -y install xvfb gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 \
       libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 \
@@ -198,8 +199,6 @@ chown -R "$AGENT_USER:$AGENT_USER" /etc/buildkite-agent/hooks
 chmod +x /etc/buildkite-agent/hooks/*
 
 gcloud auth configure-docker --quiet
-
-gcloud components install kubectl --quiet
 
 # Setup git mirrors
 {
