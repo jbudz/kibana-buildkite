ed5d34cf563a21972fd4adeb2725804e591485f9
Brian Seeders <brian.seeders@elastic.co>
2021-10-04 17:48:47 -0400

    build script should use newer cli args

--- tools/build.js
+++ tools/build.js
@@ -2,12 +2,11 @@ require('dotenv').config();
 
 const { BuildkiteClient } = require('kibana-buildkite-library');
 
+const { buildNumber, pipelineSlug } = require('./lib/getBuildFromArgs')();
+
 const client = new BuildkiteClient();
 
 (async () => {
-  const PIPELINE_SLUG = process.argv[2];
-  const BUILD_NUMBER = process.argv[3];
-
-  const build = await client.getBuild(PIPELINE_SLUG, BUILD_NUMBER);
+  const build = await client.getBuild(pipelineSlug, buildNumber, true);
   console.log(JSON.stringify(build, null, 2));
 })();
