a49bf2eb2a85034c0feafaa0cdf67410c4134770
Jonathan Budzenski <jon@budzenski.me>
2021-10-12 17:10:17 -0500

    Add OS package build dependencies

--- agents/setup.sh
+++ agents/setup.sh
@@ -19,7 +19,12 @@ apt-get install --yes \
   curl \
   gnupg-agent \
   software-properties-common \
-  buildkite-agent
+  buildkite-agent \
+  ruby \
+  ruby-dev \
+  rpm \
+
+  gem install fpm -v 1.13.1
 
 ### Get rid of Ubuntu's snapd stuff and install the Google Cloud SDK the traditional way.
 {
