483ecef06c83f348bef414570a49a7d39a181448
Jonathan Budzenski <jon@elastic.co>
2021-12-14 15:42:43 -0600

    Remove 7.15 from dev branches

--- pipelines/current-dev-branches.tf
+++ pipelines/current-dev-branches.tf
@@ -1,5 +1,5 @@
 locals {
-  current_dev_branches = ["main", "8.0", "7.16", "7.15"]
+  current_dev_branches = ["main", "8.0", "7.16"]
   hourly_branches      = ["main", "8.0", "7.16"]
   daily_branches       = setsubtract(local.current_dev_branches, local.hourly_branches)
 }
