1317f5cf34c231b96999751e772c9bdba04f7bbe
Jonathan Budzenski <jon@elastic.co>
2022-01-14 15:55:19 -0600

    Add docker context build pipeline
    Adds a pipeline for the changes introduced in
    https://github.com/elastic/kibana/pull/122897.  This will verify that
    the generated docker context builds.

---
+++ pipelines/kibana-docker-context.tf
@@ -0,0 +1,33 @@
+resource "buildkite_pipeline" "docker_context" {
+  name        = "kibana / build docker context"
+  description = "Build Docker context"
+  repository  = "https://github.com/elastic/kibana.git"
+  steps       = <<-EOT
+  env:
+    GITHUB_COMMIT_STATUS_ENABLED: 'true'
+    SLACK_NOTIFICATIONS_ENABLED: 'true'
+  steps:
+    - label: ":pipeline: Pipeline upload"
+      command: buildkite-agent pipeline upload .buildkite/pipelines/docker_context.yml
+  EOT
+
+  default_branch       = "main"
+  branch_configuration = join(" ", local.hourly_branches)
+
+  provider_settings {
+    build_branches      = false
+    build_tags          = false
+    build_pull_requests = false
+
+    trigger_mode = "none"
+  }
+}
+
+resource "buildkite_pipeline_schedule" "docker_context_daily" {
+  for_each = toset(local.hourly_branches)
+
+  pipeline_id = buildkite_pipeline.package_testing.id
+  label       = "Daily build"
+  cronline    = "0 7 * * * America/New_York"
+  branch      = each.value
+}
