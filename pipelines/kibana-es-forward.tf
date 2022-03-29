4b6c2db23cdeabf3ba1bb50a72e8aae6d953c36e
Jonathan Budzenski <jon@elastic.co>
2022-03-29 10:03:28 -0500

    Update ES forward compatibility pipeline to from 8.0 to 8.2

--- pipelines/kibana-es-forward.tf
+++ pipelines/kibana-es-forward.tf
@@ -1,11 +1,11 @@
-resource "buildkite_pipeline" "es_forward_7" {
-  name        = "kibana / 7.latest ES 8.0 forward compatibility"
+resource "buildkite_pipeline" "es_forward_7_82" {
+  name        = "kibana / 7.latest ES 8.2 forward compatibility"
   description = "Runs full CI daily using 7.latest kibana and 8.0 ES"
   repository  = "https://github.com/elastic/kibana.git"
   steps       = <<-EOT
   env:
     SLACK_NOTIFICATIONS_ENABLED: 'true'
-    ES_SNAPSHOT_MANIFEST: 'https://storage.googleapis.com/kibana-ci-es-snapshots-daily/8.0.2/manifest-latest-verified.json'
+    ES_SNAPSHOT_MANIFEST: 'https://storage.googleapis.com/kibana-ci-es-snapshots-daily/8.2.0/manifest-latest-verified.json'
   steps:
     - label: ":pipeline: Pipeline upload"
       command: buildkite-agent pipeline upload .buildkite/pipelines/hourly.yml
@@ -30,7 +30,7 @@ resource "buildkite_pipeline" "es_forward_7" {
   }
 }
 
-resource "buildkite_pipeline_schedule" "es_forward_7" {
+resource "buildkite_pipeline_schedule" "es_forward_7_82" {
   pipeline_id = buildkite_pipeline.es_forward_7.id
   label       = "Daily build"
   cronline    = "0 9 * * * America/New_York"
