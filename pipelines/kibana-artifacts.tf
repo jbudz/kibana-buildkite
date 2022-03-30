c374d99d50ae6bdd2296da46b79c2ee66dbb8039
Jonathan Budzenski <jon@elastic.co>
2022-03-30 10:56:12 -0500

    Split artifacts pipeline into separate release and snapshot pipelines (#29)
    * Split artifacts pipeline into separate release and snapshot pipelines
    
    Depends on https://github.com/elastic/kibana/pull/128801
    
    * better sort name

--- pipelines/kibana-artifacts.tf
+++ pipelines/kibana-artifacts.tf
@@ -1,10 +1,42 @@
-resource "buildkite_pipeline" "kibana_artifacts" {
-  name        = "kibana / artifacts"
-  description = "Kibana artifact builds"
+resource "buildkite_pipeline" "kibana_artifacts_snapshot" {
+  name        = "kibana / artifacts snapshot"
+  description = "Kibana snapshot artifact builds"
   repository  = "https://github.com/elastic/kibana.git"
   steps       = <<-EOT
   env:
     SLACK_NOTIFICATIONS_ENABLED: 'false'
+  steps:
+    - label: ":pipeline: Pipeline upload"
+      command: buildkite-agent pipeline upload .buildkite/pipelines/artifacts.yml
+      agents:
+        queue: kibana-default
+  EOT
+
+  default_branch       = "main"
+  branch_configuration = join(" ", local.hourly_branches)
+
+  provider_settings {
+    build_branches      = false
+    build_tags          = false
+    build_pull_requests = false
+
+    trigger_mode = "none"
+  }
+
+  team {
+    slug = "everyone"
+    access_level = "MANAGE_BUILD_AND_READ"
+  }
+}
+
+resource "buildkite_pipeline" "kibana_artifacts_release" {
+  name        = "kibana / artifacts release"
+  description = "Kibana release artifact builds"
+  repository  = "https://github.com/elastic/kibana.git"
+  steps       = <<-EOT
+  env:
+    SLACK_NOTIFICATIONS_ENABLED: 'false'
+    RELEASE_BUILD: 'true'
   steps:
     - label: ":pipeline: Pipeline upload"
       command: buildkite-agent pipeline upload .buildkite/pipelines/artifacts.yml
