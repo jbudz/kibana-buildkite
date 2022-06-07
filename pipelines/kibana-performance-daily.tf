12ef2808148cfffd1a160651b3faaf9c8444c4bf
Baturalp Gurdin <9674241+suchcodemuchwow@users.noreply.github.com>
2022-06-07 18:31:16 +0200

    Revert "Revert "continue running performance tests daily pipeline (#40)" (#41)" (#42)
    This reverts commit 3f245f118597f781ad23f17e5abf4fc695d89b65.

--- pipelines/kibana-performance-daily.tf
+++ pipelines/kibana-performance-daily.tf
@@ -23,9 +23,9 @@ resource "buildkite_pipeline" "single_user_performance" {
   }
 }
 
-# resource "buildkite_pipeline_schedule" "single_user_performance_daily" {
-#   pipeline_id = buildkite_pipeline.single_user_performance.id
-#   label       = "Single user daily test"
-#   cronline    = "0 * * * * Europe/Berlin"
-#   branch      = buildkite_pipeline.single_user_performance.default_branch
-# }
+resource "buildkite_pipeline_schedule" "single_user_performance_daily" {
+  pipeline_id = buildkite_pipeline.single_user_performance.id
+  label       = "Single user daily test"
+  cronline    = "0 * * * * Europe/Berlin"
+  branch      = buildkite_pipeline.single_user_performance.default_branch
+}
